8e962b156322a56450e28aa481e24dd198399008
tt <1109394634@qq.com>
2024-02-20 15:29:10 +0800

    update

--- src/app/[locale]/api/generate/route.ts
+++ src/app/[locale]/api/generate/route.ts
@@ -1,8 +1,4 @@
-
-
-export const apiKey = process.env.OPENAI_API_KEY || 'sk-xxxxxx'
-export const model = process.env.OPENAI_API_MODEL || 'sora-1.0-turbo'
-export const baseUrl = process.env.OPENAI_API_BASE_URL || 'https://fake-sora-api.sorawebui.com'
+import {apiKey, baseUrl, model} from "~/configs/openai";
 
 export async function POST(req: Request, res: Response) {
 
---
+++ src/configs/openai.ts
@@ -0,0 +1,3 @@
+export const apiKey = process.env.OPENAI_API_KEY || 'sk-xxxxxx'
+export const model = process.env.OPENAI_API_MODEL || 'sora-1.0-turbo'
+export const baseUrl = process.env.OPENAI_API_BASE_URL || 'https://fake-sora-api.sorawebui.com'
